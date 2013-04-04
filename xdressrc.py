a7297b93eb969d7686a354c8b745fc1b9fbb5216
Christoph Deil <Deil.Christoph@gmail.com>
2013-04-04 10:44:01 +0200

    update

---
+++ xdressrc.py
@@ -0,0 +1,13 @@
+package = 'hello'     # top-level python package name
+packagedir = 'hello'  # location of the python package
+sourcedir = 'src'      # location of C/C++ source
+
+stlcontainers = [
+    ('vector', 'str'),
+    ('set', 'uint'),
+    ('map', 'int', 'float'),
+    ]
+
+# will be used later, but need to be present now
+classes = []
+functions = []
