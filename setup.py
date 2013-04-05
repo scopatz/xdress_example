ba7c0c15e8b59fb00c6e4bef4a8747242742a2d9
Anthony Scopatz <scopatz@gmail.com>
2013-04-04 19:48:17 -0500

    made setup.py --user installable

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ import numpy as np
 incdirs = [os.path.join(os.getcwd(), 'src'), np.get_include()]
 
 ext_modules = [
-    Extension("hello.xdress_extra_types", ["hello/xdress_extra_types.pyx"], 
-              include_dirs=incdirs, language="c++"),
-    Extension("hello.stlcontainers", ["hello/stlcontainers.pyx"], 
-              include_dirs=incdirs, language="c++"),
-    ]
+  Extension("hello.xdress_extra_types", ["hello/xdress_extra_types.pyx"], 
+            include_dirs=incdirs, language="c++"),
+  Extension("hello.stlcontainers", ["hello/stlcontainers.pyx"], 
+            include_dirs=incdirs, language="c++"),
+  ]
 
 setup(
   name = 'Hello world app',
+  packages=['hello'],
   cmdclass = {'build_ext': build_ext},
   ext_modules = ext_modules,
 )
